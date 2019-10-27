4d3d37ed2bdb93efcc3d36e8a6b70f671efd64c3
Eunchurn Park <eunchurn.park@gmail.com>
2019-10-27 16:54:44 +0900

    Updates

--- index.js
+++ index.js
@@ -1,53 +1,52 @@
-'use strict';
+const { app, BrowserWindow } = require('electron')
+const path = require('path')
+const url = require('url')
 
-const { app, BrowserWindow } = require('electron');
-const path = require('path');
-const url = require('url');
+const createMenu = require('./menu')
+const checkVersion = require('./checkVersion')
 
-const createMenu = require('./menu');
-const checkVersion = require('./checkVersion');
+let win
 
-let win;
-
-function createWindow() {
+function createWindow () {
     win = new BrowserWindow({
         width: 1000,
         height: 620,
         minWidth: 1000,
         minHeight: 620
-    });
+    })
 
     win.loadURL(url.format({
         pathname: path.join(__dirname, 'index.html'),
         protocol: 'file:',
         slashes: true
-    }));
+    }))
 
     win.on('closed', () => {
-        win = null;
-    });
+        win = null
+    })
 
-    if (process.env.NODE_ENV === 'development') win.webContents.openDevTools();
+    if ( process.env.NODE_ENV === 'development' )
+        win.webContents.openDevTools()
 
     win.webContents.once('did-finish-load', () => {
-        win.isReady = true;
-    });
+        win.isReady = true
+    })
 }
 
 app.on('ready', () => {
-    createWindow();
-    createMenu();
-    checkVersion(win);
-});
+    createWindow()
+    createMenu()
+    checkVersion(win)
+})
 
 app.on('window-all-closed', () => {
     if (process.platform !== 'darwin') {
-        app.quit();
+        app.quit()
     }
-});
+})
 
 app.on('activate', () => {
     if (win === null) {
-        createWindow();
+        createWindow()
     }
-});
+})
